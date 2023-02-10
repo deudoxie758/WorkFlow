07bd486f8696f9374e9088d3dc4e2f4a5091f6e2
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-02-09 21:49:42 -0400

    message post route complete

--- src/pages/api/messages/index.js
+++ src/pages/api/messages/index.js
@@ -1,8 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 export default async function handler(req, res) {
   const method = req.method;
   const prisma = new PrismaClient();
   switch (method) {
     case "POST":
+      try {
+        const { user_id, channel_id, body } = req.body;
+        const message = await prisma.message.create({
+          data: {
+            user_id,
+            channel_id,
+            body,
+            created_at: new Date(),
+          },
+        });
+        res.status(200).json(message);
+      } catch (error) {
+        res.status(400).json({ error: error });
+      }
       break;
 
     default:

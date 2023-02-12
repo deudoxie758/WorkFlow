d92324030d25e7f895a574965047d8ebdc6220b3
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-02-12 19:23:33 -0400

    printing messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function Chat({ channel }) {
-  return <div>{channel.name}</div>;
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    setMessages(channel.messages);
+  });
+  return (
+    <div>
+      {messages ? (
+        <>
+          <div>{channel.name}</div>
+          <div>{channel.description}</div>
+          <div>
+            {messages.map((message) => (
+              <li key={message.id}>{message.body}</li>
+            ))}
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
 
 export default Chat;
